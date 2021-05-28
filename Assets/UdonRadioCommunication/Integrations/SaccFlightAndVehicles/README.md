5f10b5fbb59536e3fa6c04bf163e52f0ed0b69e6
esnya <esnya@nekometer.com>
2021-05-28 20:14:00 +0900

    :sparkles: feat: Add integration for SaccFlightAndVehicles

---
+++ Assets/UdonRadioCommunication/Integrations/SaccFlightAndVehicles/README.md
@@ -0,0 +1,9 @@
+# URC Integrations SaccFlightAndVehicles
+
+## Usage
+1. Copy a `UdonBehaviour` on `PilotSeat` in your plane.
+2. Create a new GameObject as child of `PilotSeat`.
+3. Pase as new component into created GameObject.
+4. Remove a `UdonBehaviour` on `PilotSeat`.
+5. Add new component `URCPilotSeeat` and press `Convert` button.
+6. Place GameObject which has `Tranceiver`, `Transmitter` and `Receiver` into your plane.
---
+++ Assets/UdonRadioCommunication/Integrations/SaccFlightAndVehicles/URCPilotSeat.cs
@@ -0,0 +1,92 @@
+/// Original Code Created by https://github.com/Sacchan-VRC
+/// Edited by https://github.com/esnya
+
+using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+
+namespace UdonRadioCommunication
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync), DefaultExecutionOrder(100)]
+    class URCPilotSeat : UdonSharpBehaviour
+    {
+        public Transceiver transceiver;
+        public PilotSeat originalPilotSeat;
+
+        private EngineController EngineControl;
+        private GameObject LeaveButton;
+        private GameObject Gun_pilot;
+        private GameObject SeatAdjuster;
+        private LeaveVehicleButton LeaveButtonControl;
+
+        private void Start()
+        {
+            EngineControl = originalPilotSeat.EngineControl;
+            LeaveButton = originalPilotSeat.LeaveButton;
+            Gun_pilot = originalPilotSeat.Gun_pilot;
+            SeatAdjuster = originalPilotSeat.SeatAdjuster;
+            LeaveButtonControl = LeaveButton.GetComponent<LeaveVehicleButton>();
+        }
+
+        public override void Interact()
+        {
+            EngineControl.PilotEnterPlaneLocal();
+            originalPilotSeat.EngineControl.localPlayer.UseAttachedStation();
+            if (LeaveButton != null) { LeaveButton.SetActive(true); }
+            if (Gun_pilot != null) { Gun_pilot.SetActive(true); }
+            if (SeatAdjuster != null) { SeatAdjuster.SetActive(true); }
+        }
+
+        public override void OnStationEntered(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+            {
+                transceiver.exclusive = false;
+                transceiver.Activate();
+                transceiver.StartTalking();
+            }
+
+            EngineControl.PilotEnterPlaneGlobal(player);
+            //voice range change to allow talking inside cockpit (after VRC patch 1008)
+            LeaveButtonControl.SeatedPlayer = player.playerId;
+            if (player.isLocal)
+            {
+                foreach (LeaveVehicleButton crew in EngineControl.LeaveButtons)
+                {//get get a fresh VRCPlayerAPI every time to prevent players who   left leaving a broken one behind and causing crashes
+                    VRCPlayerApi guy = VRCPlayerApi.GetPlayerById(crew.SeatedPlayer);
+                    if (guy != null)
+                    {
+                        SetVoiceInside(guy);
+                    }
+                }
+            }
+            else if (EngineControl.Piloting || EngineControl.Passenger)
+            {
+                SetVoiceInside(player);
+            }
+        }
+
+        public override void OnStationExited(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+            {
+                transceiver.StopTalking();
+                transceiver.Deactivate();
+            }
+            originalPilotSeat.PlayerExitPlane(player);
+        }
+
+        private void SetVoiceInside(VRCPlayerApi Player)
+        {
+            Player.SetVoiceDistanceNear(999999);
+            Player.SetVoiceDistanceFar(1000000);
+            Player.SetVoiceGain(.6f);
+        }
+        private void SetVoiceOutside(VRCPlayerApi Player)
+        {
+            Player.SetVoiceDistanceNear(0);
+            Player.SetVoiceDistanceFar(25);
+            Player.SetVoiceGain(15);
+        }
+    }
+}
