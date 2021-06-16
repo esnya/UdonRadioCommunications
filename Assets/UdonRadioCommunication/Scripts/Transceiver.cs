9f069d50baf32dd39a95a8278434aba64053736b
esnya <esnya@nekometer.com>
2021-06-16 12:49:57 +0900

    :sparkles: feat: Transceiver.SetFrequency to public

--- Assets/UdonRadioCommunication/Scripts/Transceiver.cs
+++ Assets/UdonRadioCommunication/Scripts/Transceiver.cs
@@ -56,6 +56,14 @@ namespace UdonRadioCommunication
             Networking.SetOwner(Networking.LocalPlayer, gameObject);
         }
 
+        public void Respawn()
+        {
+            TakeOwnership();
+            var sync = (VRCObjectSync)GetComponent(typeof(VRCObjectSync));
+            if (sync == null) return;
+            sync.Respawn();
+        }
+
         public void SetTalking(bool value)
         {
             TakeOwnership();
@@ -78,7 +86,7 @@ namespace UdonRadioCommunication
         public void Deactivate() => SetActive(false);
         public void ToggleActive() => SetActive(!active);
 
-        private void SetFrequency(float f)
+        public void SetFrequency(float f)
         {
             TakeOwnership();
             frequency = Mathf.Clamp(f, minFrequency, maxFrequency);
