1e4c60d627b42c45ae924ea8d0e173e4d1e8d1ea
esnya <esnya@nekometer.com>
2021-08-01 19:45:43 +0900

    :zap: improve(transmitter): Delay to deactivate transmitter

--- Assets/UdonRadioCommunication/Scripts/Transmitter.cs
+++ Assets/UdonRadioCommunication/Scripts/Transmitter.cs
@@ -9,23 +9,36 @@ namespace UdonRadioCommunication
     {
         [UdonSynced] public bool active;
         [UdonSynced] public float frequency = 1.0f;
-
+        public float deactivateDelay = 3.0f;
         public void TakeOwnership()
         {
             if (Networking.IsOwner(gameObject)) return;
             Networking.SetOwner(Networking.LocalPlayer, gameObject);
         }
 
+        public bool IsActive() => active;
         public void SetActive(bool value)
         {
+            if (value) Activate();
+            else Deactivate();
+        }
+        public void Activate()
+        {
             TakeOwnership();
-            active = value;
+            active = false;
             RequestSerialization();
         }
-        public bool IsActive() => active;
-        public void Activate() => SetActive(true);
-        public void Deactivate() => SetActive(false);
+        public void Deactivate()
+        {
+            SendCustomEventDelayedSeconds(nameof(_DelayedDeactivate), deactivateDelay);
+        }
 
+        public void _DelayedDeactivate()
+        {
+            TakeOwnership();
+            active = false;
+            RequestSerialization();
+        }
 
         public void SetFrequency(float f)
         {
